2e7eef6751a0dc867f5b321c4fa01089854f55e5
Andy <89816884+lya0108@users.noreply.github.com>
2024-09-04 10:53:37 +1200

    working correct, wrong, and score stats

--- G00_Quiz_v5.py
+++ G00_Quiz_v5.py
@@ -4,6 +4,7 @@ from tkinter import *
 from functools import partial  # prevents unwanted windows
 import csv
 import random
+import time
 
 # font for all buttons: Arial size 20 bold, with white text
 button_font = ("Arial", "16", "bold")
@@ -23,7 +24,7 @@ class main:
 
         self.main_instructions = Label(self.main_frame,
                                        text="Enter number of question in the \nbox below then press play",
-                                       font=("Arial", "12"), wrap=300, justify="center")
+                                       font=("Arial", "12"), justify="center")
         self.main_instructions.grid(row=1, pady=10)
 
         self.entry_frame = Frame(self.main_frame)
@@ -44,10 +45,6 @@ class main:
         self.button_frame = Frame(self.main_frame)
         self.button_frame.grid(row=5, padx=10, pady=10)
 
-        self.stats_button = Button(self.button_frame, text="Stats", font=button_font, bg="#14eceb", fg=button_fg,
-                                   activebackground="#006464", activeforeground="#ffffff", width=6, state=DISABLED)
-        self.stats_button.grid(row=0, column=2, padx=5)
-
         self.help_button = Button(self.button_frame, text="Help", font=button_font, bg="#F23390", fg=button_fg,
                                   activebackground="#b40b5e", activeforeground="#ffffff", width=6,
                                   command=self.to_help)
@@ -57,6 +54,10 @@ class main:
                                   activebackground="#8b17b1", activeforeground="#ffffff", width=6,
                                   command=lambda: self.num_question())
         self.play_button.grid(row=0, column=1, padx=5)
+
+        self.stats_button = Button(self.button_frame, text="Stats", font=button_font, bg="#14eceb", fg=button_fg,
+                                   activebackground="#006464", activeforeground="#ffffff", width=6, state=DISABLED)
+        self.stats_button.grid(row=0, column=2, padx=5)
 
     # checks user input is bigger than 0
     def num_question(self):
@@ -110,6 +111,12 @@ class Play:
         self.rounds_played = IntVar()
         self.rounds_played.set(0)
 
+        self.rounds_correct = IntVar()
+        self.rounds_correct.set(0)
+
+        self.rounds_wrong = IntVar()
+        self.rounds_wrong.set(0)
+
         # if users press cross at top, closes game
         self.play_box.protocol('WM_DELETE_WINDOW', partial(self.close_play))
 
@@ -118,40 +125,50 @@ class Play:
 
         rounds_heading = f"Question 1 of {how_many}"
         self.game_heading = Label(self.quiz_frame,
-                                  text=rounds_heading, font=("Arial", "16", "bold"))
+                                  text=rounds_heading, font=("Arial", "16", "bold"), wraplength=300)
         self.game_heading.grid(row=0)
 
-        self.question_label = Label(self.quiz_frame, text="", wrap=300, height=3)
+        self.question_label = Label(self.quiz_frame, text="", height=3, wraplength=300)
         self.question_label.grid(row=1)
 
         self.choice_frame = Frame(self.quiz_frame)  # frame for choice buttons
-        self.choice_frame.grid(row=2)
+        self.choice_frame.grid(row=2, pady=5)
 
-        self.choice_button_ref = []
+        self.choice_button_ref = []  # list used to refer to the buttons when editing config
         self.button_gods_list = []
 
         # create buttons to choose
         for item in range(0, 4):
             self.choice_button = Button(self.choice_frame,
-                                        width=18, wrap=100, disabledforeground="#000000", height=2)
+                                        width=18, disabledforeground="#000000", height=2, wraplength=125)
 
             self.choice_button.grid(row=item // 2, column=item % 2, padx=10, pady=5)
 
             self.choice_button_ref.append(self.choice_button)
 
-        self.help_next_frame = Frame(self.quiz_frame)
-        self.help_next_frame.grid(row=3)
+        self.stats_next_frame = Frame(self.quiz_frame, bg="#ffe291")
+        self.stats_next_frame.grid(row=3)
 
-        self.help_button = Button(self.help_next_frame, text="Help", font=button_font, bg="#F23390", fg=button_fg,
-                                  activebackground="#b40b5e", activeforeground="#ffffff", width=10,
-                                  command=self.to_help)
-        self.help_button.grid(row=0, column=0, padx=10)
+        self.stats_correct = Label(self.stats_next_frame, text="Correct: 0", bg="#ffe291")
+        self.stats_correct.grid(row=0, column=0, padx=16)
 
-        self.next_button = Button(self.help_next_frame, text="Next", font=button_font, bg="#00bfff",
+        self.stats_wrong = Label(self.stats_next_frame, text="Wrong: 0", bg="#ffe291")
+        self.stats_wrong.grid(row=0, column=1, padx=16)
+
+        self.next_button = Button(self.stats_next_frame, text="Next", font=button_font, bg="#00bfff",
                                   fg=button_fg,
                                   activebackground="#0086b3", activeforeground="#ffffff",
-                                  width=10, state=DISABLED, command=lambda: self.new_round(difficulty))
-        self.next_button.grid(row=0, column=1, padx=10)
+                                  width=8, state=DISABLED, command=lambda: self.new_round(difficulty))
+        self.next_button.grid(row=0, column=2)
+
+        self.score_time_frame = Frame(self.quiz_frame, bg="#ffe291")
+        self.score_time_frame.grid(row=4, pady=10)
+
+        self.stats_score = Label(self.score_time_frame, text=f"Score: 0%", bg="#ffe291")
+        self.stats_score.grid(row=0, column=0, padx=46, pady=10)
+
+        self.stats_time = Label(self.score_time_frame, text=f"Time: 0s", bg="#ffe291")
+        self.stats_time.grid(row=0, column=1, padx=46)
 
         self.new_round(difficulty)
 
@@ -206,8 +223,8 @@ class Play:
             self.choice_button_ref[i]["text"] = self.button_gods_list[i][1]
             self.choice_button_ref[i]["state"] = NORMAL
 
-            # checks which answer is correct and enable next button
-            self.choice_button_ref[i].config(command=lambda i=i: self.check_answer(i, correct_index,current_round))
+            # checks if answer is correct and enable next button
+            self.choice_button_ref[i].config(command=lambda i=i: self.check_answer(i, correct_index, current_round))
 
             # Determine if this button is the correct answer
             if i == correct_index:
@@ -217,14 +234,25 @@ class Play:
                 else:
                     self.question_label.config(text=f"Who is the {self.button_gods_list[i][2]}")
 
-    def check_answer(self, clicked_index, correct_index,current_round):
+    def check_answer(self, clicked_index, correct_index, current_round):
 
         if clicked_index == correct_index:
             # Handle correct answer logic here
             self.choice_button_ref[clicked_index]["bg"] = "#2ebb01"
+            num_correct = self.rounds_correct.get()
+            num_correct += 1
+            self.rounds_correct.set(num_correct)
+            self.stats_correct.config(text=f"Correct: {self.rounds_correct.get()}")
+
+            score = num_correct / self.rounds_wanted.get() * 100
+            self.stats_score.config(text=f"Score: {score:.1f}%")
         else:
             # Handle wrong answer logic here
             self.choice_button_ref[clicked_index]["bg"] = "#CF3C49"
+            num_wrong = self.rounds_wrong.get()
+            num_wrong += 1
+            self.rounds_wrong.set(num_wrong)
+            self.stats_wrong.config(text=f"Wrong: {self.rounds_wrong.get()}")
 
         for i in range(len(self.choice_button_ref)):
             self.choice_button_ref[i]["state"] = DISABLED
@@ -234,9 +262,6 @@ class Play:
             self.next_button.config(command=lambda: self.close_play())
         else:
             self.next_button.config(state=NORMAL)
-
-    def to_help(self):
-        DisplayHelp(self)
 
     def close_play(self):
         # reshow root and end current game
@@ -269,8 +294,7 @@ class DisplayHelp:
                    "game will tell you whether you're correct or not. Then press 'Next' to move on\n\nNormal " \
                    "Difficulty: Default\nHard Difficulty: removes pantheon hint in question"
 
-        self.help_info = Label(self.help_frame, text=helpinfo, font=("Arial", "12"), bg=background, wrap=300,
-                               justify=LEFT)
+        self.help_info = Label(self.help_frame, text=helpinfo, font=("Arial", "12"), bg=background, justify=LEFT, wraplength=300)
         self.help_info.grid(row=1, padx=10, pady=10, column=0)
 
         self.return_button = Button(self.help_frame, text="Return", font=("Arial", "15", "bold"), bg="#a30000",
